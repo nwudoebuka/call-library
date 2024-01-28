04103d9b4ebdc076cdf56c93c0c69233556a5aa6
Anthony <anthony@AP24DKM070.ROOMS-DK>
2024-01-28 22:09:12 +0100

    added an extra tag for maven-publish to gradle file

--- call-library/build.gradle.kts
+++ call-library/build.gradle.kts
@@ -73,7 +73,6 @@ publishing {
     }
 
     repositories {
-        mavenCentral()
         mavenLocal()
     }
 }
@@ -87,3 +86,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+apply(plugin = "maven-publish")
